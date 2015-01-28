fc7f060c0efd76712c149a0a1c19e7842b25c122
sahidulislam <sahidul.islam@rootnext.com>
2015-01-28 20:58:14 +0600

    friend search and add friend continue

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,19 +10,39 @@ class UsersController < ApplicationController
     # @msgs_recipient=current_user.received_messages.where("sender_id = ?",1)
     # @newarry=@msgs_recipient+@msgs_sent
     # @sortedarr= @newarry.sort_by &:created_at
-    @friend=current_user.sent_request.where("friend_type = ?",1)
+    friend1=current_user.sent_request.where("friend_type = ?",1)
+    friend2=current_user.received_request.where("friend_type = ?",1)
     @friend_list=[]
-    @friend.each do |frnd|
-      temp_user=User.find(frnd.recipient_id)
-      @friend_list<<temp_user
+    if friend1.any?
+      friend1.each do |frnd|
+        temp_user=User.find(frnd.recipient_id)
+        @friend_list<<temp_user
+      end
     end
-
-
+    if friend2.any?
+      friend2.each do |frnd|
+        temp_user=User.find(frnd.sender_id)
+        @friend_list<<temp_user
+      end
+    end
   end
 
   def show
     @user=User.find(params[:id])
-    # frndtype=current_user.sent_request
+    @friend_comment='Add Friend'
+    frndtype1=current_user.sent_request.where("friend_type = ? AND recipient_id = ?", 1 , params[:id])
+    frndtype2=current_user.received_request.where("friend_type = ? AND sender_id = ?", 1, params[:id])
+    frndtype3=current_user.received_request.where("friend_type = ? AND sender_id = ?", 2, params[:id])
+    frndtype4=current_user.sent_request.where("friend_type = ? AND recipient_id = ?", 2, params[:id])
+    if frndtype1.any? || frndtype2.any?
+      @friend_comment='Friend'
+    end
+    if frndtype3.any?
+      @friend_comment='Confirm or Cancel'
+    end
+    if frndtype4.any?
+      @friend_comment='Request Sent'
+    end
   end
   def new
     @user=User.new
